a96279f8f87651b7600b1ec24572121b67bbd263
John Walker <johnwalker00@gmail.com>
2018-01-28 16:58:11 -0600

    Routine Check-in

--- script.js
+++ script.js
@@ -37,8 +37,8 @@ var whiteMove = function () {
     movingnow = 'white';
     //var bestMove = getBestMove(game, whiteDepth, minimaxRootWhite);
     //var bestMove = randomMove(game);
-    var bestMove = randomCapture(game);
-    //var bestMove = bestCapture(game);
+    //var bestMove = randomCapture(game);
+    var bestMove = bestCapture(game);
     //var bestMove = avoidBeingCaptured(game);
 
     game.ugly_move(bestMove);
@@ -62,8 +62,8 @@ var whiteMove = function () {
 var blackMove = function () {
     movingnow = 'black';
     //var bestMove = getBestMove(game, blackDepth, minimaxRoot);
-    var bestMove = randomMove(game);
-    //var bestMove = randomCapture(game);
+    //var bestMove = randomMove(game);
+    var bestMove = randomCapture(game);
     //var bestMove = bestCapture(game);
     game.ugly_move(bestMove);
     board.position(game.fen());
